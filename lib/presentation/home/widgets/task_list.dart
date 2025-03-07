4b35e018a99f3242d8d37c8cec3ae3e351963096
pulo0 <bartexx13042005@gmail.com>
2025-03-07 20:55:46 +0100

    feat: add swipping list tiles to delete

--- lib/presentation/home/widgets/task_list.dart
+++ lib/presentation/home/widgets/task_list.dart
@@ -2,19 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:task_todo_app/presentation/home/widgets/list_tile_card.dart';
 import 'package:task_todo_app/styles/app_padding.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
   const TaskList({super.key});
 
+  @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
   @override
   Widget build(BuildContext context) {
     final tempCount = List<String>.generate(100, (i) => 'Item $i');
     return Padding(
       padding: AppPadding.medium,
       child: ListView.builder(
-        itemBuilder: (context, index) => ListTileCard(
-          key,
-          items: tempCount,
-          currentIndex: index,
+        itemBuilder: (context, index) => Dismissible(
+          background: Container(color: Colors.red.shade800),
+          key: ValueKey<String>(tempCount[index]),
+          onDismissed: (DismissDirection direction) {
+            setState(() => tempCount.removeAt(index));
+          },
+          child: ListTileCard(
+            widget.key,
+            items: tempCount,
+            currentIndex: index,
+          ),
         ),
         physics: const NeverScrollableScrollPhysics(),
         shrinkWrap: true,
